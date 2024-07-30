785b9a621e55bdc4d7048a7d060d730c9e7613a9
Torycd <vicidundun@gmail.com>
2024-07-31 00:14:29 +0100

    yes

--- strengthchecker.py
+++ strengthchecker.py
@@ -4,3 +4,4 @@
 #     - Check password length, complexity (uppercase, lowercase, numbers, special characters), and similarity to common passwords
 #     - Provide feedback (weak, medium, strong)
 #     - Optionally, compare input password with stored hashes (e.g., common passwords)
+
