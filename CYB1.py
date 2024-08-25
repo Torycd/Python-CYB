9416ec819f20f43cd9c2289f5624fb516867661d
Torycd <vicidundun@gmail.com>
2024-08-25 18:11:01 +0100

    lets go

--- CYB1.py
+++ CYB1.py
@@ -1,3 +1,16 @@
 with open("python_analyze.txt", "r") as file:
     file_text = file.read()
-print(file_text)    
+username = file_text.split()
+# print(username)
+
+def login_check(login_list, current_user):
+    counter = 0
+    for i in login_list:
+        if i == current_user:
+            counter = counter + 1
+    if counter >= 3:
+        return "you have tried to login three or more times. your account has been locked"
+    else:
+        return "You can login"
+
+login_check(username, "Liam")
